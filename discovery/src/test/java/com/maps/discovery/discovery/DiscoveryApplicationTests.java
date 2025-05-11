dee73641639825afad54830204a7e06e3829b576
saurabh chauhan <sourabh1202@gmail.com>
2025-05-11 23:52:13 +0300

    added discovery server

---
+++ discovery/src/test/java/com/maps/discovery/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maps.discovery.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
