cbf700a3e25c0bea2c7524a90c8567a0324806d4
saurabh chauhan <sourabh1202@gmail.com>
2025-06-05 21:49:39 +0300

    removed js server

--- server/prisma/migrations/20240928180149_create_floor_data_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "FloorData" (
-    "id" SERIAL NOT NULL,
-    "line" TEXT NOT NULL,
-    "location" TEXT NOT NULL,
-
-    CONSTRAINT "FloorData_pkey" PRIMARY KEY ("id")
-);
--- server/utility/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-    baseURL: process.env.MAP_PROCESSOR_URL,
-    });
-
-console.log(process.env.MAP_PROCESSOR_URL);
-
-export default axiosInstance;
